414d8f8b471c0b8d8530cf1205a56f734e93e7c8
abarron2 <andibarron7@gmail.com>
2023-01-19 20:15:24 -0800

    Added resource tracking to GameManager

--- Assets/scripts/GameManager.cs
+++ Assets/scripts/GameManager.cs
@@ -5,12 +5,27 @@ using UnityEngine.UI;
 using System;
 using UnityEngine.SceneManagement;
 
+//this will keep track of stats for player across scenes. Also helpful for testing since stats can be changed with sliders
+[System.Serializable]
+public class playerStats
+{
+    public float maxHealth;
+    public float movementSpeed;
+    public float basicDamage;
+    public float attackSpeed;
+    public float cooldownReduction;
+}
+
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
 
+    public float timer;
+    public Text timerText;
     public bool roomCleared;
-
+    public int currentGold; 
+    public List<String> availableAbilities = new List<String>(); //not sure how we will keep track of abilities yet but a list of strings to hold ablities that can be learned
+    
     //setup singleton
     private void Awake() {
         instance = this;
@@ -20,21 +35,22 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        timer = 0;
         roomCleared = false;
+        currentGold = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
+        timer += Time.deltaTime;
+        Debug.Log("Time: " + timer.ToString("F2")); //timer displays in console for now
+
         //Checks to see if enemies are still in arena
         Enemy[] enemies = FindObjectsOfType<Enemy>();
-        if(enemies.Length ==0){
-            print("NO ENEMIES");
+        if(enemies.Length == 0){
             roomCleared = true; 
+            //Add some code to advance to next scene
         }
-        else{
-            print("YES ENEMIES");
-        }
-
     }
 }
