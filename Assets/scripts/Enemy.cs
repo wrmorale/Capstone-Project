4a9e0d4ab9b002e8b1b0a6efc49e28fd60a9b147
Miguel Cedillo <82360408+migyced@users.noreply.github.com>
2023-01-19 20:22:07 -0800

    Added a simple arena, obstacle/enemy, and stairs

--- Assets/scripts/Enemy.cs
+++ Assets/scripts/Enemy.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    public int maxHealth = 2;
+    public int health;
     // Start is called before the first frame update
     void Start()
     {
-        
+       health = maxHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown("space"))
+        {
+            health--;
+            if(health <= 0)
+            {
+                // Destroy the cube when it has no health left
+                Destroy(gameObject);
+            }
+        }
     }
 }
