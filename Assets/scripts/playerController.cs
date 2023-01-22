a571ba10d48b1e2b0594e279b0627851592ffb3e
vderbier <victor.derbier@gmail.com>
2023-01-21 17:05:18 -0800

    cleaned up code + comments

--- Assets/scripts/playerController.cs
+++ Assets/scripts/playerController.cs
@@ -1,6 +1,8 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+// This code is based on sample unity movement API code.
+
 [RequireComponent(typeof(CharacterController), typeof(PlayerInput))]
 public class playerController : MonoBehaviour
 {
@@ -10,11 +12,9 @@ public class playerController : MonoBehaviour
     private float jumpHeight = 1.0f;
     [SerializeField]
     private float gravityValue = -9.81f;
-     [SerializeField]
-    private float rotationSpeed = 1f;
-     public float turnSmoothTime = 0.1f;
+    [SerializeField]
+    public float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
-
 
     private CharacterController controller;
     private PlayerInput playerInput;
@@ -29,36 +29,42 @@ public class playerController : MonoBehaviour
         controller = GetComponent<CharacterController>();
         playerInput = GetComponent<PlayerInput>();
         cam = Camera.main.transform;
-        moveAction = playerInput.actions["Move"];
+        moveAction = playerInput.actions["Move"]; 
+        // add actions from playerControls here
     }
 
     void Update()
     {
         groundedPlayer = controller.isGrounded;
-        if (groundedPlayer && playerVelocity.y < 0)
-        {
+        if (groundedPlayer && playerVelocity.y < 0){
             playerVelocity.y = 0f;
         }
 
         // store direction input 
         Vector2 input = moveAction.ReadValue<Vector2>();
-        Vector3 move = new Vector3(input.x, 0, input.y);
-        // move according to camera angle
-        move = move.x * cam.right.normalized + move.z * cam.forward.normalized;
-        move.y = 0f;
-        controller.Move(move * Time.deltaTime * playerSpeed);
 
-        // Changes the height position of the player..
-        if (Input.GetButtonDown("Jump") && groundedPlayer)
-        {
+        // if there is movement input 
+        if (input.x != 0 || input.y != 0){
+            Vector3 move = new Vector3(input.x, 0, input.y);
+            
+            // calculate model rotation
+            float targetAngle = Mathf.Atan2(input.x, input.y) * Mathf.Rad2Deg + cam.eulerAngles.y; // from front facing position to direction pressed + camera angle.
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f); // apply rotation
+
+            // move according to calculated target angle
+            move = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(move * Time.deltaTime * playerSpeed);
+        }
+
+        // Changes the height position of the player.. 
+        // TODO: change jump to action input
+        if (Input.GetButtonDown("Jump") && groundedPlayer){
             playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
         }
 
+        // add gravity
         playerVelocity.y += gravityValue * Time.deltaTime;
         controller.Move(playerVelocity * Time.deltaTime);
-
-        // rotate towards camera angle
-        Quaternion targetRotation = Quaternion.Euler(0, cam.eulerAngles.y, 0);
-        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, rotationSpeed);
     }
 }
