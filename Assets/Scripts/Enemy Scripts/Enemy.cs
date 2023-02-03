a059b2a0a47f19a1ba33370f55d6a5d2021ab07c
abarron2 <andibarron7@gmail.com>
2023-02-02 21:34:06 -0800

    Added Enemy Idle Movement

--- Assets/Scripts/Enemy Scripts/Enemy.cs
+++ Assets/Scripts/Enemy Scripts/Enemy.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [Header("stats")]
-    [SerializeField] private double maxHealth;
-    public double health;
-    [SerializeField] private float movementSpeed;
+    [SerializeField]private string enemyName;
+    [SerializeField]private double maxHealth;
+    [SerializeField]public double health;
+    [SerializeField]public float basicAttackDamage;
+    [SerializeField]private float movementSpeed;
+    public List<Ability> abilities; 
 
     
     void Start(){
@@ -19,10 +22,6 @@ public class Enemy : MonoBehaviour
     }
 
     public void isHit(){
-        //checks to make sure enemy hasn't already been deleted
-        if(this == null){
-            return;
-        }
         health-=1.0f;
         if(health <= 0){
             // Destroy the cube when it has no health left
--- Assets/Scripts/Enemy Scripts/enemyMovement.cs
+++ Assets/Scripts/Enemy Scripts/enemyMovement.cs
@@ -6,10 +6,14 @@ public class enemyMovement : MonoBehaviour
 {
     [SerializeField] private float speed;
     [SerializeField] private float range;
+    [SerializeField] private float idleMovementRange;
     [SerializeField] private Transform player;
     private Rigidbody body;
     private Rigidbody playerBody;
     private Vector3 movement;
+    private Vector3 idleMovement;
+    private float elapsedTime = 0;
+    private bool isIdle = false;
 
     private void Awake() {
         body = GetComponent<Rigidbody>();
@@ -24,8 +28,20 @@ public class enemyMovement : MonoBehaviour
             body.MovePosition(body.position + (movement * Time.fixedDeltaTime));
         }
         else {
-            //enemy is idle
-            movement = Vector2.zero;
+            //enemy idle movement
+            elapsedTime += Time.deltaTime;
+            if (elapsedTime >= 3f) {
+                elapsedTime = 0;
+                isIdle = !isIdle;
+                if (isIdle) {
+                    idleMovement = body.position + new Vector3(Random.Range(-idleMovementRange, idleMovementRange), 0, Random.Range(-idleMovementRange, idleMovementRange));
+                    movement = (idleMovement - body.position).normalized * speed;
+                } 
+                else {
+                    movement = Vector3.zero;
+                }
+            }
+            body.MovePosition(body.position + (movement * Time.fixedDeltaTime));
         }
     }
 }
