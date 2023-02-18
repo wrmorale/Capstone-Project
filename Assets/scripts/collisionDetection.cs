d5e654d6432a998ab7fe5bdec32cfec731a84c79
Miguel Cedillo <miacedil@ucsc.edu>
2023-02-17 20:27:09 -0800

    Added the dust piles and made them hittable, they also become smaller every time they're hit

--- Assets/scripts/collisionDetection.cs
+++ Assets/scripts/collisionDetection.cs
@@ -8,12 +8,18 @@ public class collisionDetection : MonoBehaviour
     public playerController pc;
     public Player player;
     public Enemy enemy;
+    public DustPile dustPile;
     
     private void OnTriggerEnter(Collider other){
         if(other.tag == "Enemy" && pc.state == States.PlayerStates.Attacking){
             //applies to all with the enemy tag
             Enemy enemy = other.GetComponent<Enemy>();
             enemy.isHit(player.basicDamage);
+        }
+
+        if(other.tag == "DustPile" && pc.state == States.PlayerStates.Attacking){
+            DustPile dustPile = other.GetComponent<DustPile>();
+            dustPile.isHit(player.basicDamage);
         }
     }
 }
