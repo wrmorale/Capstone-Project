65fe98cf1aae8035850159ca432e8a10fbd4ae59
Miguel Cedillo <miacedil@ucsc.edu>
2023-03-09 17:58:49 -0800

    Revert "unneccesary code"
    This reverts commit 1b0302ab2c900c5226087ba84b08073ec3afc8c8.

--- Assets/scripts/Enemy Scripts/DamageFlash.cs
+++ Assets/scripts/Enemy Scripts/DamageFlash.cs
@@ -13,6 +13,11 @@ public class DamageFlash : MonoBehaviour{
         origColor = mesh.material.GetColor("_MainTex_ST");
     }
 
+    void Update(){
+        if(Input.GetKeyDown(KeyCode.Space)){
+            FlashStart();
+        }
+    }
 
     public void FlashStart(){
         mesh.material.SetColor("_MainTex_ST", Color.red);
