f2309b21b850e4cffb3bee85e6e8b5c7e80caa0a
Jackson <jackson.e.gerard@gmail.com>
2023-03-17 18:33:16 -0700

    Added keyboard controls, and modified other menu screens

--- Assets/UI Toolkit/Panels/KeyboardPresenter.cs
+++ Assets/UI Toolkit/Panels/KeyboardPresenter.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 using UnityEngine.UIElements;
 using UnityEngine.SceneManagement;
 
-public class ControlPresenter : MonoBehaviour
+public class KeyboardPresenter : MonoBehaviour
 {
     private void Awake() {
         VisualElement root = GetComponent<UIDocument>().rootVisualElement;
+        foreach (GameObject gameManager in GameObject.FindGameObjectsWithTag("GameManager"))
+            Destroy(gameManager);
         root.Q<Button>("BackButton").clicked += () => SceneManager.LoadScene("Title_Scene");
-        root.Q<Button>("KeyboardButton").clicked += () => SceneManager.LoadScene("KeyboardControls_Scene");
         // root.Q<Button>("Start").clicked += () => Debug.Log("test");
     }
 }
